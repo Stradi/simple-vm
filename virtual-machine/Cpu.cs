a8314376fe453f4eb8a2ed8e73b68067e0dfe86c
Batin EVIRGEN <batinevirgen@gmail.com>
2017-07-04 00:26:41 +0300

    Added simple Cpu class and some instructions to run

---
+++ virtual-machine/Cpu.cs
@@ -0,0 +1,51 @@
+﻿namespace Bat.Vm {
+    public class Cpu {
+        public const int DEFAULT_STACK_SIZE = 1000;
+        
+        private int[] stack = new int[DEFAULT_STACK_SIZE];
+
+        public void RunCode(int[] code) {
+            int instructionPointer = 0;
+            int stackPointer = -1;
+
+            int a;
+            int b;
+            int result;
+
+            Instructions opcode = (Instructions)code[instructionPointer];
+
+            while (opcode != Instructions.EXIT) {
+                instructionPointer++;
+
+                switch (opcode) {
+                    case Instructions.ADD:
+                        b = stack[stackPointer--];
+                        a = stack[stackPointer--];
+                        result = a + b;
+                        stack[++stackPointer] = result;
+                        break;
+                    case Instructions.SUB:
+                        b = stack[stackPointer--];
+                        a = stack[stackPointer--];
+                        result = a - b;
+                        stack[++stackPointer] = result;
+                        break;
+                    case Instructions.MUL:
+                        b = stack[stackPointer--];
+                        a = stack[stackPointer--];
+                        result = a * b;
+                        stack[++stackPointer] = result;
+                        break;
+                    case Instructions.PUSH:
+                        stack[++stackPointer] = code[instructionPointer++];
+                        break;
+                    case Instructions.POP:
+                        --stackPointer;
+                        break;
+                }
+
+                opcode = (Instructions)code[instructionPointer];
+            }
+        }
+    }
+}
---
+++ virtual-machine/Instructions.cs
@@ -0,0 +1,10 @@
+﻿namespace Bat.Vm {
+    public enum Instructions {
+        ADD = 1,
+        SUB = 2,
+        MUL = 3,
+        PUSH = 4,
+        POP = 5,
+        EXIT = 6
+    };
+}
